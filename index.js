69d7acb62878e319cccde4e17c0958744f7ac48a
chengying <hollye2558@gmail.com>
2022-11-18 21:28:22 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length; i++) {
+    if (word.charAt(i) !== word.charAt(word.length-1-i)){
+      return false;
+    }
+  }
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
+  iterate through the word
+
+  if the  letter at index  is not the same as the letter at index word.length-1-i
+  then return false
+
+  if it iterates through every character of the word, and it is all the same
+
+  then return true
 */
 
 /*
   Add written explanation of your solution here
+
+  use for loop to iterate through the word.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
+  console.log("Expecting abc: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
